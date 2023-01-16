719e1af2ee23ee90dea6b8a6c52acb03d12ca63a
pouryourlove <lqu134@naver.com>
2023-01-16 23:59:32 +0900

    delete

--- starter/script.js
+++ starter/script.js
@@ -58,10 +58,7 @@ console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
 //if first value is truthy value, JS doesn't need to look at the second value. It just return the first value
 
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
-
+restaurant.numGuests = 0;
 const guests2 = restaurant.numGuests || 10;
 console.log(guests2);
 
