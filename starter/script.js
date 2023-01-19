debe015599618d456cfae8d5ec368d9fabaa5390
pouryourlove <lqu134@naver.com>
2023-01-19 19:05:25 +0900

    optional chaining

--- starter/script.js
+++ starter/script.js
@@ -1,64 +1,90 @@
 'use strict';
 
-// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-// const openingHours = {
-//   openingHours: {
-//     [weekdays[3]]: {
-//       open: 12,
-//       close: 22,
-//     },
-//     [weekdays[4]]: {
-//       open: 11,
-//       close: 23,
-//     },
-//     [weekdays[5]]: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-// };
-
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-//   //ES6 enhanced object literals
-//   openingHours,
-
-//   order(starterIndex, mainIndex) {
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//   },
-
-//   orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-//     console.log(
-//       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-//     );
-//   },
-
-//   orderPasta(ing1, ing2, ing3) {
-//     console.log(`Here is your delicious pasta with${ing1},${ing2} and ${ing3}`);
-//   },
-//   orderPizza(mainIngredient, ...otherIngredients) {
-//     console.log(mainIngredient);
-//     console.log(otherIngredients);
-//   },
-// };
-
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 20,
-
-  numGuests: 0,
+const openingHours = {
+  openingHours: {
+    [weekdays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    [weekdays[4]]: {
+      open: 11,
+      close: 23,
+    },
+    [weekdays[5]]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 };
 
-const rest2 = {
-  name: 'La Pizza',
-  owner: 'Giovanni Rossi',
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  //ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with${ing1},${ing2} and ${ing3}`);
+  },
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+//WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+//Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Mehtods
+console.log(restaurant.order?.(0, 1) ?? 'Merhod does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Merhod does not exist');
+
+//Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+
+console.log(users[0]?.name ?? 'User array empty');
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
+
+// const rest1 = {
+//   name: 'Capri',
+//   // numGuests: 20,
+
+//   numGuests: 0,
+// };
+
+// const rest2 = {
+//   name: 'La Pizza',
+//   owner: 'Giovanni Rossi',
+// };
 
 //OR assignment operator
 // rest1.numGuests = rest1.numGuests || 10;
@@ -67,16 +93,16 @@ const rest2 = {
 // rest2.numGuests ||= 10;
 
 //nullish assignment operator(null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
 // rest1.owner = rest1.owner && '<ANONYMOUS>';
 // rest2.owner = rest2.owner && '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
 ////The Nullish Coalescing Operator/////
 // // Use ANY data type, return Any data type, short-circuiting
